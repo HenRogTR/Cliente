d390e3e40ad5bd465fe81c013e90ffa45d46583f
Henrri Rogger Segundo Trujillo Romero <htrujill@emeal.nttdata.com>
2022-06-23 11:33:46 -0500

    Añadiendo dependencia jpa y entity

--- src/main/java/pe/com/cliente/model/Cliente.java
+++ src/main/java/pe/com/cliente/model/Cliente.java
@@ -1,5 +1,46 @@
 package pe.com.cliente.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "cliente")
 public class Cliente {
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "codigo")
+	private String codigoUnico;
+
+	@Column(name = "nombres")
+	private String nombres;
+
+	@Column(name = "apellidos")
+	private String apellidos;
+
+	@Column(name = "tipo_documento")
+	private String tipoDocumento;
+
+	@Column(name = "numero_documento")
+	private String numeroDocumento;
+
+	@Column(name = "fecha_registro")
+	@CreatedDate
+	private Date fechaRegistro;
+
 }
